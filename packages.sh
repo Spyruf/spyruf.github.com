cab042dca685a876ce7985f0d59dc37e52eb6067
Spyruf <spyruf@gmail.com>
2015-08-03 18:30:03 -0400

    added OVO Zeppelin Logo Package

--- packages.sh
+++ packages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/Spyruf\ Lion\ Zeppelin\ Logo debs
+dpkg-deb -bZgzip projects/OVO\ Zeppelin\ Logos debs
 #dpkg-deb -bZgzip projects/<package name> <output folder>
