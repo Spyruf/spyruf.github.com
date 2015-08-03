27d1384325d24c928795598e698dba37bebcc7a8
Spyruf <spyruf@gmail.com>
2015-08-03 18:32:13 -0400

    small fix

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/Spyruf\ Lion\ Zeppelin\ Logo debs
-dpkg-deb -bZgzip projects/OVO\ Zeppelin\ Logos debs
+dpkg-deb -bZgzip projects/OVO\ Zeppelin\ Logos Zeppelin\ Logo debs
 #dpkg-deb -bZgzip projects/<package name> <output folder>
