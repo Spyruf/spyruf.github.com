1d6a9f9a672ba998fb9ec92aecaac7e3b231434c
Spyruf <spyruf@gmail.com>
2015-08-04 18:50:08 -0400

    fix

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/projects//com.spyruf.OVOZeppelinLogos debs
-dpkg-deb -bZgzip projects//com.spyruf.SpyrufLionZeppelinLogo debs
+dpkg-deb -bZgzip projects/com.spyruf.OVOZeppelinLogos debs
+dpkg-deb -bZgzip projects/com.spyruf.SpyrufLionZeppelinLogo debs
 #dpkg-deb -bZgzip projects/<package name> <output folder>
