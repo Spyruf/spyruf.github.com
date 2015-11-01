637c1de4996d9f7ffadfff9023540ba9c6e738b4
Spyruf <spyruf@gmail.com>
2015-11-01 14:31:28 -0500

    changed to NoCCKnobs

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/com.spyruf.OVOZeppelinLogos debs
 dpkg-deb -bZgzip projects/com.spyruf.SpyrufLionZeppelinLogo debs
-dpkg-deb -bZgzip projects/com.spyruf.NoCCSliders debs
+dpkg-deb -bZgzip projects/com.spyruf.NoCCKnobs debs
 
 #dpkg-deb -bZgzip projects/<package name> <output folder>
