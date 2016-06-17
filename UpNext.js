f6d1c1584e86c82d6c3e9a270284f32bd90aad64
Spyruf <spyruf@gmail.com>
2016-06-16 20:43:31 -0400

    gmusic files

--- UpNext.js
+++ UpNext.js
@@ -31,6 +31,12 @@
 
         $scope.init = function () {
 
+            //gmusic
+
+            //window.gmusic = new window.GMusic(window); // Our Google Music API
+            //window.gmusic.volume.getVolume(); // 50 (ranges from 0 to 100)
+
+            //hotkeys  
             hotkeys.add({
                 combo: 'space',
                 description: 'Play/Pause',
