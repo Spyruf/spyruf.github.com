27397ae04a263e76f7c037cefd3a711c28fee1e3
Spyruf <spyruf@gmail.com>
2016-06-17 00:15:56 -0400

    gmusic

--- upnext/UpNext.js
+++ upnext/UpNext.js
@@ -33,7 +33,7 @@
 
             //gmusic
 
-            //window.gmusic = new window.GMusic(window); // Our Google Music API
+            window.gmusic = new window.GMusic(window); // Our Google Music API
             //window.gmusic.volume.getVolume(); // 50 (ranges from 0 to 100)
 
             //hotkeys  
