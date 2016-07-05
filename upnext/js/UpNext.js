b077e9808db2c35bb4e4a0a016b6d7981424bb18
Spyruf <spyruf@gmail.com>
2016-07-04 21:25:46 -0400

    reorganized file structure
    much neater now

--- upnext/js/UpNext.js
+++ upnext/js/UpNext.js
@@ -7,13 +7,19 @@
 
     app.controller('UpNextController', ['$scope', '$filter', '$firebaseArray', '$firebaseObject', 'hotkeys', function ($scope, $filter, $firebaseArray, $firebaseObject, hotkeys) {
 
-
-
         $scope.ab = {
             index: 0,
             //isPlaying: false,
+            //time: 0,
+        };
+
+        $scope.cd = {
+            //index: 0,
+            //isPlaying: false,
             time: 0,
         };
+
+
 
         $scope.userCount = 0;
 
@@ -21,8 +27,69 @@
         $scope.myText = "";
         $scope.myPlayer;
 
+        //jQuery
+        $(document).ready(function () {
+
+            /** Check if mobile device - for disabling popovers
+             * jQuery.browser.mobile (http://detectmobilebrowser.com/)
+             *
+             * jQuery.browser.mobile will be true if the browser is (not) a mobile device
+             *
+             **/
+            (function (a) {
+                (jQuery.browser = jQuery.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))
+            })(navigator.userAgent || navigator.vendor || window.opera);
+
+            $("#helpHome").click(function () {
+                $("#myModal2").modal();
+            });
+
+            $("#help").click(function () {
+
+                $("#myModal").modal();
+            });
+
+
+            if (jQuery.browser = true) {
+                $('[data-toggle="tooltip"]').tooltip()
+            }
+
+
+            if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+                //alert("It's Safari");
+            } else {
+
+
+                $("#uID").click(function () {
+                    $('#uID').attr('data-original-title', 'Copied!').tooltip('fixTitle').tooltip('show');
+
+                });
+
+                $("#uID").hover(function () {}, function () {
+                    $('#uID').attr('data-original-title', 'Click to copy Playlist ID!').tooltip('fixTitle');
+                });
+
+                $("#link").click(function () {
+                    $('#link').attr('data-original-title', 'Copied!').tooltip('fixTitle').tooltip('show');
+                });
+
+                $("#link").hover(function () {}, function () {
+                    $('#link').attr('data-original-title', "Click to copy this playlist's link").tooltip('fixTitle');
+                });
+
+                //alert(jQuery.browser);
+            }
+
+        });
+        //
+
+
+
+
         $scope.init = function () {
 
+
+            //hotkeys
             hotkeys.add({
                 combo: 'space',
                 description: 'Play/Pause',
@@ -36,7 +103,7 @@
 
             hotkeys.add({
                 combo: 'left',
-                description: 'Previous Track (Press twice if a track has already started)',
+                description: 'Previous Track',
                 callback: function () {
                     if (document.activeElement.id != "searchInput") {
                         $scope.prev();
@@ -50,6 +117,11 @@
                 callback: function () {
                     if (document.activeElement.id != "searchInput") {
                         $scope.next();
+
+                        //var temp = $scope.gmu.volume.getVolume();
+                        //temp = temp.toString();
+                        console.log("right is clicked");
+                        //                        console.log(temp);
                     }
                 }
             });
@@ -57,7 +129,7 @@
 
             $scope.link = location.href;
 
-            //shows model or something like that 
+            //shows model or something like that
             $(document).ready(function () {
                 $("#myModal").modal('show');
             });
@@ -84,11 +156,23 @@
                     $scope.ab = {
                         index: 0,
                         //isPlaying: false,
-                        time: 0,
+                        //time: 0,
                     }
                 }
             });
 
+            var refTime = new Firebase('https://amber-heat-3079.firebaseio.com/' + $scope.uID.toString() + '/cd'); //uID/ab
+            $scope.c = $firebaseObject(refTime); //$scope.a is NOT used, instead $scope.ab is used
+
+            $scope.c.$bindTo($scope, 'cd').then(function () {
+                if ($scope.cd.time == null) {
+                    $scope.cd = {
+                        //index: 0,
+                        //isPlaying: false,
+                        time: 0,
+                    };
+                }
+            });
 
             //--------
 
@@ -372,7 +456,7 @@
                     $scope.onIDS.push($scope.queue[$scope.ab.index].id);
 
                     $scope.myPlayer.on('time', function () {
-                        $scope.ab.time = $scope.myPlayer.currentTime();
+                        $scope.cd.time = $scope.myPlayer.currentTime();
                         $scope.$apply();
                     });
 
@@ -400,12 +484,12 @@
                 //only play the song if music was already playing
                 if ($scope.isPlaying == true) {
 
-                    if ($scope.ab.time != 0) {
-                        $scope.myPlayer.seek($scope.ab.time); //4-16
+                    if ($scope.cd.time != 0) {
+                        $scope.myPlayer.seek($scope.cd.time); //4-16
                         $scope.$apply;
                     }
 
-                    //console.log("in the update funct" + $scope.ab.time)
+                    //console.log("in the update funct" + $scope.cd.time)
 
                     $scope.myPlayer.play();
                     //console.log("plays the new song");
@@ -420,23 +504,27 @@
         $scope.prev = function () {
 
 
-            if ($scope.myPlayer != null && $scope.myPlayer.currentTime() > 1000 && $scope.queue.length != 0) {
-                //                $scope.ab.index = $scope.ab.index; // if a song has started, keep the index the same - don't need to do anything
+            //
+            //            if ($scope.myPlayer != null && $scope.myPlayer.currentTime() > 2000 && $scope.queue.length != 0) {
+            //                console.log('test');
+            //                //                $scope.ab.index = $scope.ab.index; // if a song has started, keep the index the same - don't need to do anything
+            //
+            //            } else {
+            //                var temp = $scope.ab.index;
+            //                temp = temp - 1;
+            //                $scope.ab.index = temp; // if a song hasn't started then go to the previous song
 
-            } else {
-                //                var temp = $scope.ab.index;
-                //                temp = temp - 1;
-                //                $scope.ab.index = temp; // if a song hasn't started then go to the previous song
-
-                $scope.ab.index = $scope.ab.index - 1;
-                //console.log("prevving" + $scope.ab.index);
+            $scope.ab.index = $scope.ab.index - 1;
+            //console.log("prevving" + $scope.ab.index);
 
 
-            }
+            //            }
 
             if ($scope.ab.index < 0) {
                 $scope.ab.index = 0; // can't go earlier than the first song
             }
+
+
 
             if ($scope.queue[$scope.ab.index] != null) {
 
@@ -449,6 +537,9 @@
                 $scope.disabled = false;
 
             }
+
+
+
 
 
         }
@@ -483,7 +574,6 @@
             }
 
 
-
             //updates the displayed queue
             $scope.display = $filter('limitTo')($scope.queue, $scope.queue.length, $scope.ab.index + 1);
 
@@ -493,6 +583,10 @@
             //            6-7
 
             $scope.artwork();
+
+
+
+
 
         }
 
@@ -515,10 +609,10 @@
                     $scope.myPlayer.pause();
 
 
-                    //conditionals 
+                    //conditionals
 
                     $scope.myPlayer.on('time', function () {
-                        $scope.ab.time = $scope.myPlayer.currentTime();
+                        $scope.cd.time = $scope.myPlayer.currentTime();
                         $scope.$apply();
                     });
 
@@ -532,9 +626,9 @@
                         //$scope.myPlayer.seek(80000);
                         console.log("setTimeout");
 
-                        if ($scope.ab.time != 0) {
-                            console.log($scope.ab.time);
-                            $scope.myPlayer.seek($scope.ab.time);
+                        if ($scope.cd.time != 0) {
+                            console.log($scope.cd.time);
+                            $scope.myPlayer.seek($scope.cd.time);
                         } // CHK4
 
                         $scope.myPlayer.play();
@@ -592,9 +686,9 @@
             }
 
 
-            if ($scope.ab.time != 0) {
-                console.log($scope.ab.time);
-                $scope.myPlayer.seek($scope.ab.time);
+            if ($scope.cd.time != 0) {
+                console.log($scope.cd.time);
+                $scope.myPlayer.seek($scope.cd.time);
             } // CHK4
 
             $scope.myPlayer.play();
@@ -617,7 +711,7 @@
                 $scope.art = $scope.queue[$scope.ab.index].artwork.replace("large", "t500x500");
 
             } else {
-                $scope.art = "Template.png"
+                $scope.art = "img/Template.png"
             }
             //title
             if ($scope.ab.index < $scope.queue.length && $scope.queue[$scope.ab.index].title != null) {
@@ -630,52 +724,97 @@
 
         $scope.copyUID = function () {
 
-            // Create a "hidden" input
-            var aux = document.createElement("input");
 
-            // Assign it the value of the specified element
-            aux.setAttribute("value", document.getElementById("uID").innerHTML); // main line
+            if ((navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) || jQuery.browser == false) {
+                //alert("It's Safari");
 
-            // Append it to the body
-            document.body.appendChild(aux);
+                // get the input
+                var aux = document.getElementById("copyInput");
 
-            // Highlight its content
-            aux.select();
+                // Assign it the value of the specified element
+                aux.setAttribute("value", document.getElementById("uID").innerHTML); // main line
+                aux.value = document.getElementById("uID").innerHTML;
 
-            // Copy the highlighted text
-            document.execCommand("copy");
 
-            // Remove it from the body
-            document.body.removeChild(aux);
+                // Highlight its content
+                //                aux.focus();
+                //                setTimeout(function () {
+                //                    aux.setSelectionRange(0, 9999);
+                //                }, 1);
+                //                aux.focus();
+                //                aux.select();
 
+                $("#copyModal").modal();
+
+
+            } else {
+                // Create a "hidden" input
+                var aux = document.createElement("input");
+
+                // Assign it the value of the specified element
+                aux.setAttribute("value", document.getElementById("uID").innerHTML); // main line
+
+                // Append it to the body
+                document.body.appendChild(aux);
+
+                // Highlight its content
+                aux.select();
+
+                // Copy the highlighted text
+                document.execCommand("copy");
+
+                // Remove it from the body
+                document.body.removeChild(aux);
+            }
 
         }
 
         $scope.copyLink = function () {
 
-            // Create a "hidden" input
-            var aux = document.createElement("input");
+            if ((navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) || jQuery.browser == false) {
+                //alert("It's Safari");
 
-            // Assign it the value of the specified element
-            aux.setAttribute("value", $scope.link); // main line
+                // get the input
+                var aux = document.getElementById("copyInput");
 
-            // Append it to the body
-            document.body.appendChild(aux);
+                // Assign it the value of the specified element
+                aux.setAttribute("value", $scope.link); // main line
+                aux.value = $scope.link;
 
-            // Highlight its content
-            aux.select();
+                // Highlight its content
+                //                aux.focus();
+                //                setTimeout(function () {
+                //                    aux.setSelectionRange(0, 9999);
+                //                }, 1);
+                //                aux.focus();
+                //                aux.select();
 
-            // Copy the highlighted text
-            document.execCommand("copy");
+                $("#copyModal").modal();
 
-            // Remove it from the body
-            document.body.removeChild(aux);
+
+
+            } else {
+                // Create a "hidden" input
+                var aux = document.createElement("input");
+
+                // Assign it the value of the specified element
+                aux.setAttribute("value", $scope.link); // main line
+
+                // Append it to the body
+                document.body.appendChild(aux);
+
+                // Highlight its content
+                aux.select();
+
+                // Copy the highlighted text
+                document.execCommand("copy");
+
+                // Remove it from the body
+                document.body.removeChild(aux);
+            }
 
 
         }
-
-
-
 
 
     }]);
