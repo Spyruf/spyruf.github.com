845af8b92532a92f43e2352738222eb13c272165
Spyruf <spyruf@gmail.com>
2016-10-01 13:29:48 -0400

    updated EQ Lab script

--- Equilibrium_Lab/main.js
+++ Equilibrium_Lab/main.js
@@ -1,9 +1,4 @@
-/**
- * Created by rahulbatra on 9/30/16.
- */
-
-var app = angular.module('main', []);
-
+app = angular.module('main', []);
 app.controller('mainController', function ($scope) {
 
 
@@ -13,7 +8,7 @@ app.controller('mainController', function ($scope) {
     $scope.s1 = .757; //diameter straw 1 - bigger straw (cm)
     $scope.s2 = .593;  //diameter straw 2 - smaller straw (cm)
 
-    $scope.trials = 5; // number of times it has to be right
+    $scope.trials = 3; // number of times it has to be right
     $scope.sigFigs = 3; // number of sigFigs the values have to be equal to
 
     $scope.h1Array = [];
@@ -23,25 +18,30 @@ app.controller('mainController', function ($scope) {
 
 
     $scope.cycle = function () {
+        document.getElementById('output').innerHTML = "";
 
-        var x = 0;
-        while(x < 100) {
+        while ($scope.similar < $scope.trials) {
 
             $scope.v1 = Math.PI * Math.pow(($scope.s1 / 2), 2) * $scope.h1; // volume transferred from straw 1 // pi  * r^2 * h (ml  or cm^3)
             $scope.v2 = Math.PI * Math.pow(($scope.s2 / 2), 2) * $scope.h2; // volume transferred from straw 2 // pi  * r^2 * h (ml  or cm^3)
 
-            $scope.h1 =  $scope.h1 - $scope.v1 + $scope.v2;
-            $scope.h2 =  $scope.h2 - $scope.v2 + $scope.v1;
+            $scope.h1 = $scope.h1 - $scope.v1 + $scope.v2;
+            $scope.h2 = $scope.h2 - $scope.v2 + $scope.v1;
 
             console.log("Height 1: " + $scope.h1 + " Height 2: " + $scope.h2);
 
-            document.getElementById('output').innerHTML += '<br>' + "Height 1: " + $scope.h1 + " Height 2: " + $scope.h2;
+            document.getElementById('output').innerHTML += '<br>' + "Height 1: " + $scope.h1 + " Height 2: " + $scope.h2 + " Volume taken from Straw 1: " + $scope.v1 + " Volume taken from Straw 2: " + $scope.v2;
+
+
+            if (($scope.h1Array[$scope.h1Array.length - 1] - $scope.h1) < 0.001 && ($scope.h2Array[$scope.h2Array.length - 1] - $scope.h2) < 0.001)
+                $scope.similar++;
 
             $scope.h1Array.push($scope.h1);
             $scope.h2Array.push($scope.h2);
-
-            x++;
         }
+
+        document.getElementById('output').innerHTML = "Trials needed to reach equilibrium: " + $scope.h1Array.length  + document.getElementById('output').innerHTML;
+
     }
 
 });
