8b59d1c98866f418426ada2686748fb7bb3f1707
Rahul Batra <spyruf@gmail.com>
2017-12-02 18:46:14 -0500

    fixed all issues and now allows you to choose what sites to search

--- script.user.js
+++ script.user.js
@@ -1,69 +1,66 @@
 // ==UserScript==
 // @name         WebAssign Searcher
 // @namespace    http://tampermonkey.net/
-// @version      5.1
+// @version      9.0
 // @description  Automatically Googles the questions on a WebAssign assingment for you
 // @author       Rahul Batra
 // @include      http://www.webassign.net/web/Student/Assignment-Responses/*
 // @grant        none
-// @require http://code.jquery.com/jquery-latest.js http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css
-// @require http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css
+// @require http://code.jquery.com/jquery-latest.js
 
 // ==/UserScript==
-
-// Testing update
 
 (function() {
   'use strict';
 
-  // Sites to search
-  var sites = ""
-
-  var dialog = $('<p>WebAssign Searcher:\nWhat sites do you want to search on?</p>').dialog({
-    buttons: {
-      "Yahoo Answers": function() {
-        sites = "site:answers.yahoo.com"
-        console.log('Searching Yahoo Answers');
-        dialog.dialog('close');
-      },
-      "Chegg": function() {
-        sites = "site:chegg.com"
-        console.log('Searching Chegg');
-        dialog.dialog('close');
-      },
-      "Google": function() {
-        console.log('Searching Both');
-        dialog.dialog('close');
-      },
-      "Don't search": function() {
-        console.log('Not searching');
-        dialog.dialog('close');
-        return;
-      }
-    }
-  });
-
   console.log("window.location.href is: " + window.location.href);
   console.log("WebAssign Searcher it running!!!");
 
-  var qs = $(".studentQuestionContent").map(function() {
-    return this.innerHTML;
-  }).get();
 
-  for (var x = 0; x < qs.length; x++) {
-    qs[x] = qs[x].replace(/<(?:.|\n)*?>/gm, '');
-    qs[x] = qs[x].trim();
-    qs[x] = qs[x].replace(/(^[ \t]*\n)/gm, "");
+  // Sites to search
+  // 0 = None
+  // 1 = Google
+  // 2 = Only Yahoo Answers
+  // 3 = Only Chegg
 
-  }
+  // Default Google
+  var sites = 1;
 
-  // Questions
-  console.log(qs);
+  sites = prompt("What sites would you like to search for answers? \n 0 = Don't Search \n 1 = Google \n 2 = Only Yahoo Answers \n 3 = Only Chegg");
 
-  var google = "https://www.google.com/search?q=";
+  console.log(sites);
+  if (sites == "0" || sites != "1" && sites != "2" && sites != "3")
+    console.log("exiting");
+  else {
+    if (sites == "1") {
+      sites = "";
+    }
+    else if(sites == "2") {
+      sites = " site:answers.yahoo.com";
+    }
+    else if(sites == "3") {
+      sites = " site:chegg.com";
+    }
 
-  for (x = 0; x < qs.length; x++) {
-    var tab = window.open(google + qs[x] + sites, '_blank');
+    var qs = $(".studentQuestionContent").map(function() {
+      return this.innerHTML;
+    }).get();
+
+    for (var x = 0; x < qs.length; x++) {
+      qs[x] = qs[x].replace(/<(?:.|\n)*?>/gm, '');
+      qs[x] = qs[x].trim();
+      qs[x] = qs[x].replace(/(^[ \t]*\n)/gm, "");
+
+    }
+
+    // Questions
+    console.log(qs);
+
+    var google = "https://www.google.com/search?q=";
+
+    for (x = 0; x < qs.length; x++) {
+      var tab = window.open(google + qs[x] + sites, '_blank');
+    }
   }
 
 })();
