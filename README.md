e6aca5af7976949c9b0c8275bc39a877c520801a
Rahul Batra <spyruf@gmail.com>
2017-12-02 16:07:43 -0500

    edited readme, added userscript

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WebAssign-Searcher
 A simple JS userscript to automatically google WebAssign questions.
+
+- Goes through the page and gets each questions
+- Opens a new window with each questions googled
+- No need to waste time copying and pasting each question!
+
+# Installation
+1. To install this script get Tampermonkey (chrome/firefox) or another UserScript extension for your broswer
+2. Install script from Greasyfork.org so that you always have the latest version -
---
+++ userscript.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         WebAssign Searcher
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Automaticqsy googles qs the questions on a WebAssign assingment for you
+// @author       Rahul Batra, Harun Feraidon
+// @include      http://www.webassign.net/web/Student/Assignment-Responses/*
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+
+// ==/UserScript==
+
+
+
+(function() {
+  'use strict';
+
+  console.log("window.location.href is: " + window.location.href);
+  console.log("WebAssign Searcher it running!!!");
+
+  var qs = $(".studentQuestionContent").map(function() {
+    return this.innerHTML;
+  }).get();
+
+  for (var x = 0; x < qs.length; x++) {
+    qs[x] = qs[x].replace(/<(?:.|\n)*?>/gm, '');
+    qs[x] = qs[x].trim();
+    qs[x] = qs[x].replace(/(^[ \t]*\n)/gm, "");
+
+  }
+
+  // Questions
+  console.log(qs);
+
+  var google = "https://www.google.com/search?q=";
+
+  for (x = 0; x < qs.length; x++) {
+    var tab = window.open(google + qs[x], '_blank');
+  }
+
+})();
