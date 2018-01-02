77960a9b5294f714ebfb601031646fb69d41d87a
Rahul Batra <spyruf@gmail.com>
2018-01-02 16:14:54 -0500

    fixed small issue when answer is found

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WebAssign Searcher
 // @namespace    http://tampermonkey.net/
-// @version      11.0
+// @version      11.1
 // @description  Automatically Googles the questions on a WebAssign assingment for you
 // @author       Rahul Batra
 // @include      http://www.webassign.net/web/Student/Assignment-Responses/*
@@ -91,6 +91,8 @@
             // check if PAV question is the in localstorage string of all questions
             if (localStorage.getItem("qs").indexOf(qs[0]) !== -1) {
                 alert("found answers");
+		localStorage.setItem('pav', '0');
+		return;
             }
             else {
                 console.log("Trying again");
