76c87d335d2e49ea3bbfe63cf1d992ce706dcb5f
Rahul Batra <spyruf@gmail.com>
2018-01-02 15:09:48 -0500

    allocated space for refresh code

--- script.user.js
+++ script.user.js
@@ -29,58 +29,65 @@
     // Default Google
     var sites = 1;
 
-    sites = prompt("What sites would you like to search for answers? \n 0 = Don't Search \n 1 = Google \n 2 = Only Yahoo Answers \n 3 = Only Chegg");
 
-
-    var q = document.getElementsByClassName("studentQuestionContent");
-
-    var qs = [];
-
-    for (var x = 0; x < q.length; x++) {
-        qs[x] = q[x].innerHTML;
-        qs[x] = qs[x].replace(/<(?:.|\n)*?>/gm, '');
-        qs[x] = qs[x].replace('&nbsp;', '');
-        qs[x] = qs[x].trim();
-        qs[x] = qs[x].replace(/(^[ \t]*\n)/gm, '');
-
-    }
-
-    // Questions
-    console.log(qs);
-    localStorage.setItem("qs", qs);
-
-    console.log(sites);
-    if (sites === null || sites === "0" || sites !== "1" && sites !== "2" && sites !== "3") {
-        console.log("exiting");
+    if (window.location.href.indexOf("practice") !== -1) {
+        // PAV Code
     }
     else {
-        if (sites === "1") {
-            sites = "";
-        } else if (sites === "2") {
-            sites = " site:answers.yahoo.com";
-        } else if (sites === "3") {
-            sites = " site:chegg.com";
+
+        sites = prompt("What sites would you like to search for answers? \n 0 = Don't Search \n 1 = Google \n 2 = Only Yahoo Answers \n 3 = Only Chegg");
+
+
+        var q = document.getElementsByClassName("studentQuestionContent");
+
+        var qs = [];
+
+        for (var x = 0; x < q.length; x++) {
+            qs[x] = q[x].innerHTML;
+            qs[x] = qs[x].replace(/<(?:.|\n)*?>/gm, '');
+            qs[x] = qs[x].replace('&nbsp;', '');
+            qs[x] = qs[x].trim();
+            qs[x] = qs[x].replace(/(^[ \t]*\n)/gm, '');
+
+        }
+
+        // Questions
+        console.log(qs);
+        localStorage.setItem("qs", qs);
+
+        console.log(sites);
+        if (sites === null || sites === "0" || sites !== "1" && sites !== "2" && sites !== "3") {
+            console.log("exiting");
+        }
+        else {
+            if (sites === "1") {
+                sites = "";
+            } else if (sites === "2") {
+                sites = " site:answers.yahoo.com";
+            } else if (sites === "3") {
+                sites = " site:chegg.com";
+            }
+
+
+            console.log("Opening Tabs");
+
+            var google = "https://www.google.com/search?q=";
+
+            for (x = 0; x < qs.length; x++) {
+
+                var url = google + qs[x].split(" ").splice(0, 32).join(" ") + sites;
+                console.log(url);
+
+                // Chrome
+                var tab = window.open(url, '_blank');
+
+                // Safari
+
+            }
         }
 
 
-        console.log("Opening Tabs");
-
-        var google = "https://www.google.com/search?q=";
-
-        for (x = 0; x < qs.length; x++) {
-
-            var url = google + qs[x].split(" ").splice(0, 32).join(" ") + sites;
-            console.log(url);
-
-            // Chrome
-            var tab = window.open(url, '_blank');
-
-            // Safari
-
-        }
     }
-
-    return;
 
 
 })();
