272f1d1a6261b3bad709b0d6ecec08f7b4a0a16b
Rahul Batra <spyruf@gmail.com>
2018-01-02 16:03:48 -0500

    implemented brute force method for 20 trials for Practice Another Version

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WebAssign Searcher
 // @namespace    http://tampermonkey.net/
-// @version      10.5
+// @version      10.7
 // @description  Automatically Googles the questions on a WebAssign assingment for you
 // @author       Rahul Batra
 // @include      http://www.webassign.net/web/Student/Assignment-Responses/*
@@ -29,9 +29,79 @@
     // Default Google
     var sites = 1;
 
+    if (localStorage.getItem("pav") === null) {
+
+        //console.log("settinglocalstorage");
+
+        localStorage.setItem('pav', '0');
+        console.log(localStorage.getItem('pav'));
+    }
+
 
     if (window.location.href.indexOf("practice") !== -1) {
+
+        if (localStorage.getItem('pav') === '0' && prompt("Brute Force? Type Yes or No").toUpperCase() === "NO") {
+            console.log("leaving");
+            return;
+        }
+
+        console.log("starting");
+        console.log(localStorage.getItem('pav'));
+
+        var trials = '20'; // has to be a string
+
+        var q = document.getElementsByClassName("studentQuestionContent");
+        var qs = [];
+
+        for (var x = 0; x < q.length; x++) {
+            qs[x] = q[x].innerHTML;
+            qs[x] = qs[x].replace(/<(?:.|\n)*?>/gm, '');
+            qs[x] = qs[x].replace('&nbsp;', '');
+            qs[x] = qs[x].trim();
+            qs[x] = qs[x].replace(/(^[ \t]*\n)/gm, '');
+
+        }
         // PAV Code
+        console.log("Question is: " + qs[0]);
+
+        // Fill Blanks
+        var htmlString = document.getElementsByTagName('html')[0].innerHTML;
+
+        var b = htmlString.match(/[A-Z][A-Z]_\d*_\d_\d_\d*/g);
+        var y = 0;
+        while (y < b.length) {
+            document.getElementById(b[y]).value = "12";
+            y += 1;
+        }
+
+        // Check if values are still 12
+        if (document.getElementById(b[0]).value === "12") {
+
+            document.querySelectorAll("input[type='submit']")[0].click(); // Grade This
+
+            // if (document.querySelectorAll("input[type='submit']")[1].innerHTML === "Show Answer")
+            document.querySelectorAll("input[type='submit']")[1].click(); // Show Answer
+
+            if (localStorage.getItem('pav') === trials) {
+                alert("Could not find answers \n You will be prompted to brute force again");
+                localStorage.setItem('pav', '0');
+                return;
+            }
+
+            // check if PAV question is the in localstorage string of all questions
+            if (localStorage.getItem("qs").indexOf(qs[0]) !== -1) {
+                alert("found answers");
+            }
+            else {
+                console.log("Trying again");
+                var new_pav = parseInt(localStorage.getItem("pav")) + 1;
+                localStorage.setItem('pav', new_pav.toString());
+                document.querySelectorAll("input[type='submit']")[2].click(); // Try Again
+
+            }
+        }
+
+
     }
     else {
 
